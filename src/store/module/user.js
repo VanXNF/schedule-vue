be0392db3a22cc16f71b46f8091d76b3a21075a0
zhaoning <15255783555@163.com>
2020-12-07 22:31:27 +0800

    fix log in

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -83,8 +83,8 @@ export default {
           password
         }).then(res => {
           const data = res.data
-          commit('setToken', data.token)
-          commit('setUserId', data.userId)
+          // commit('setToken', data.token)
+          commit('setUserId', data.data)
           resolve()
         }).catch(err => {
           reject(err)
@@ -110,23 +110,24 @@ export default {
     },
     // 获取用户相关信息
     getUserInfo ({ state, commit }) {
-      return new Promise((resolve, reject) => {
-        try {
-          getUserInfo(state.token).then(res => {
-            const data = res.data
-            commit('setAvatar', data.avatar)
-            commit('setUserName', data.name)
-            commit('setUserId', data.user_id)
-            commit('setAccess', data.access)
-            commit('setHasGetInfo', true)
-            resolve(data)
-          }).catch(err => {
-            reject(err)
-          })
-        } catch (error) {
-          reject(error)
-        }
-      })
+      // return new Promise((resolve, reject) => {
+      //   try {
+      //     getUserInfo(state.token).then(res => {
+      //       const data = res.data
+      //       commit('setAvatar', data.avatar)
+      //       commit('setUserName', data.name)
+      //       commit('setUserId', data.user_id)
+      //       commit('setAccess', data.access)
+      //       commit('setHasGetInfo', true)
+      //       resolve(data)
+      //     }).catch(err => {
+      //       reject(err)
+      //     })
+      //   } catch (error) {
+      //     reject(error)
+      //   }
+      // })
+      return new Promise()
     },
     // 此方法用来获取未读消息条数，接口只返回数值，不返回消息列表
     getUnreadMessageCount ({ state, commit }) {
