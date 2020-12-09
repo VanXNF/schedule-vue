8f681152e4970379ffda173b26268d5bcd442b6b
zhaoning <15255783555@163.com>
2020-12-09 20:24:08 +0800

    fix schedule

--- src/components/schedule/schedule-min.vue
+++ src/components/schedule/schedule-min.vue
@@ -6,7 +6,7 @@
         <Icon type="md-calendar"></Icon>
           {{pinSchedule.schedule_name}}
         </p>
-      <Progress :percent="90" :stroke-color="pinSchedule.bar_color" />
+      <Progress :percent="percentValue" :stroke-color="pinSchedule.bar_color" />
     </Card>
     </template>
   </div>
@@ -32,7 +32,18 @@ export default {
     // return Math.ceil(
     //   ((parseInt(pinSchedule.cur_point)) - (parseInt(pinSchedule.start_point))) / ((parseInt(pinSchedule.end_point)) - (parseInt(pinSchedule.start_point)))
     // ) * 100
-      return 90
+      if (this.pinSchedule.point_unit === ' ') {
+        const s = new Date(this.pinSchedule.start_point)
+        const c = new Date()
+        const e = new Date(this.pinSchedule.end_point)
+        const f = Math.floor((e - s) / (24 * 3600 * 1000))
+        const ss = Math.floor((c - s) / (24 * 3600 * 1000))
+        return Math.floor(ss / f * 100)
+      } else {
+        const s = parseFloat(this.pinSchedule.cur_point) - parseFloat(this.pinSchedule.start_point)
+        const e = parseFloat(this.pinSchedule.end_point) - parseFloat(this.pinSchedule.start_point)
+        return Math.floor(s / e * 100)
+      }
     }
   }
 }
