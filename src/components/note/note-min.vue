6f68536770ab8ec2f1823159edd44a9ca4d4e8c1
zhaoning <15255783555@163.com>
2020-12-11 22:39:30 +0800

    fix note

--- src/components/note/note-min.vue
+++ src/components/note/note-min.vue
@@ -10,7 +10,7 @@
       <div>
         <ul>
          <p v-for="(item, i ) in todoList" :key="i" class="ivu-c-note-card-li">
-          <Checkbox v-model="item.is_check" @on-change="handleChange(item)"></Checkbox>
+          <Checkbox v-model="item.is_check" @on-change="handleChange(item)" ></Checkbox>
           <span>{{item.todo_item_name}}</span>
           </p>
         </ul>
@@ -40,22 +40,32 @@ export default {
     handleChange (item) {
       if (item.is_check) {
         const i = this.todoList.indexOf(item)
-        deleteTodoItem({
-          user_id: this.$store.state.user.userId,
-          note_id: this.noteId,
-          todo_id: this.pinNote.todo_id,
-          todo_item_id: this.todoList[i].todo_item_id
-        }).then(
-          res => getData(res)
-        )
-          .then(
-            res => {
-              if (res.code === 'OK') {
-                this.$Message.success(res.message)
-                this.todoList.splice(i, 1)
-              }
-            }
-          )
+        // this.todoList.splice(i, 1)
+        // const str = ''
+        // let c = ''
+        // for (let index = 0; index < this.todoList.length; index++) {
+        //   const element = this.todoList[index];
+
+        // }
+        // deleteTodoItem({
+        //   user_id: this.$store.state.user.userId,
+        //   note_id: this.noteId,
+        //   todo_id: this.pinNote.todo_id,
+        //   todo_item_id: this.todoList[i].todo_item_id
+        // })
+        //   .then(
+        //     res => getData(res)
+        //   )
+        //   .then(
+        //     res => {
+        //       if (res.code === 'OK') {
+        //         this.$Message.success(res.message)
+        //         this.todoList.splice(i, 1)
+        //       }
+        //     }
+        //   )
+        this.$Message.success('成功')
+        this.todoList.splice(i, 1)
       }
     }
   },
